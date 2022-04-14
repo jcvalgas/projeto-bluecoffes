de2fb6ed0db242d69a632859d8a15f12b305456b
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-04-14 20:01:41 -0300

    initial config express, cors

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-im
+import express from "express"
+import cors from "cors"
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const port = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+    res.send('Olá')
+});
+
+app.listen(port, () => {
+    console.log(`Rodando em http://localhost:${port}`);
+})
