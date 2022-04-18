ce1a19d22f226be5830dba58b6f46b874fe5ac21
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-04-18 17:36:24 -0300

    add routes post, put and delete

--- src/routes/coffee.route.js
+++ src/routes/coffee.route.js
@@ -5,3 +5,6 @@ export const routes = express.Router()
 
 routes.get('/find-coffees', findAllCoffeesController)
 routes.get('/find-coffees/:id', findByIdCoffeeController)
+route.post('/create', paletasController.createPaletaController);
+route.put('/update/:id', paletasController.updatePaletaController);
+route.delete('/delete/:id', paletasController.deletePaletaController);
