7887834357561d4361b4bebfd6dd56a97e6a2021
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-04-19 16:51:52 -0300

    add views & fix bugs

--- src/services/coffee.service.js
+++ src/services/coffee.service.js
@@ -4,42 +4,42 @@ const coffees = [
         sabor: 'Metropolitan Express',
         preco: 10,
         descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
-        foto: 'assets/img/coffee-images/large-coffee_ccexpress.png'
+        foto: '../assets/img/coffee-images/large-coffee_ccexpress.png'
     },
     {
         id: 2,
         sabor: 'Citzen Express',
         preco: 5,
         descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
-        foto: './assets/img/coffee-images/mug-coffee_ccexpress.png'
+        foto: '../assets/img/coffee-images/mug-coffee_ccexpress.png'
     },
     {
         id: 3,
         sabor: 'Irish Coffee',
         preco: 8,
         descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
-        foto: './assets/img/coffee-images/coffee-with-cream2_ccexpress.png',
+        foto: '../assets/img/coffee-images/coffee-with-cream2_ccexpress.png',
     },
     {
         id: 4,
         sabor: 'Ristretto',
         preco: 3,
         descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
-        foto: './assets/img/coffee-images/simple-coffee_ccexpress.png'
+        foto: '../assets/img/coffee-images/simple-coffee_ccexpress.png'
     },
     {
         id: 5,
         sabor: 'Caffè corretto',
         preco: 12,
         descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
-        foto: './assets/img/coffee-images/coffee-with-cream1_ccexpress.png'
+        foto: '../assets/img/coffee-images/coffee-with-cream1_ccexpress.png'
     },
     {
         id: 6,
         sabor: 'Caffè Latte',
         preco: 15,
         descricao: 'tempor incididunt ut labore et dolore magna aliqua.',
-        foto: './assets/img/coffee-images/large-coffee_ccexpress.png'
+        foto: '../assets/img/coffee-images/large-coffee_ccexpress.png'
     }
 ]
 
